e1e4777850f9c8fa980539d0d279955ff4833d4d
patw10 <62399846+patw10@users.noreply.github.com>
2020-05-28 16:08:45 +0200

    1.0
    1.0

---
+++ Assets/Scripts/Opendoor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Opendoor : MonoBehaviour
+{
+    public SpriteRenderer door;
+    public Sprite doorOpen;
+    public Sprite mysprite;
+    private SpriteRenderer toggle;
+    void Start()
+    {
+        toggle = GetComponent<SpriteRenderer>();
+    }
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            toggle.sprite = mysprite;
+            door.sprite = doorOpen;
+        }
+    }
+}
---
+++ Assets/Scripts/PM.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PM : MonoBehaviour
+{
+    public float Ps = 5;
+    private float hDir;
+    [SerializeField] private LayerMask Base;
+    public float jumpVelocity = 9f;
+    private Rigidbody2D _rigidBody2D;
+    private CapsuleCollider2D circleC2D;
+    void Awake()
+    {
+        _rigidBody2D = transform.GetComponent<Rigidbody2D>();
+        circleC2D = transform.GetComponent<CapsuleCollider2D>();
+    }
+    void Update()
+    {
+        hDir = Input.GetAxisRaw("Horizontal");
+        transform.position += new Vector3(hDir, 0, 0) * Time.deltaTime * Ps;
+
+        Vector3 chScale = transform.localScale;
+
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            chScale.x = -1f;
+        }
+        else if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            chScale.x = 1f;
+        }
+        transform.localScale = chScale;
+
+        if (isGround() && Input.GetKeyDown(KeyCode.Space))
+        {
+            _rigidBody2D.velocity = Vector2.up * jumpVelocity;
+        }
+    }
+    private bool isGround()
+    {
+        RaycastHit2D rayC2D = Physics2D.BoxCast(circleC2D.bounds.center, circleC2D.bounds.size, 0f, Vector2.down, .1f, Base);
+
+        return rayC2D.collider != null;
+    }
+}
